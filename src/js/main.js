f6a2649cae67869a5efbb73c3fcb8353a32b7508
unknown <boris.galac@gmail.com>
2024-07-24 01:27:27 +0200

    asd

--- src/js/main.js
+++ src/js/main.js
@@ -7,10 +7,13 @@ hamBtn.addEventListener("click", (e) => {
   if (nav.getAttribute("aria-expanded") === "false") {
     nav.setAttribute("aria-expanded", "true");
     hamBtn.setAttribute("data-active", "true");
+    document.querySelector(".main").classList.add("body-overlay");
     // header.setAttribute("data-active", "false");
   } else {
     nav.setAttribute("aria-expanded", "false");
     hamBtn.setAttribute("data-active", "false");
+    document.querySelector(".main").classList.remove("body-overlay");
+
     // header.setAttribute("data-active", "true");
   }
   nav.addEventListener("click", (e) => {
@@ -19,6 +22,16 @@ hamBtn.addEventListener("click", (e) => {
       hamBtn.setAttribute("data-active", "false");
     }
   });
+});
+
+document.body.addEventListener("click", (e) => {
+  if (nav.getAttribute("aria-expanded") === "true") {
+    if (e.target.matches(".body-overlay")) {
+      nav.setAttribute("aria-expanded", "false");
+      hamBtn.setAttribute("data-active", "false");
+      document.querySelector(".main").classList.remove("body-overlay");
+    }
+  }
 });
 
 // PROJECT COUNTER
@@ -53,6 +66,8 @@ var swiper = new Swiper(".testimonial-swiper", {
   },
 });
 
+// INPUT FOCUS
+
 const contactInputAll = document.querySelectorAll(".contact__form-input input");
 const contactLabelAll = document.querySelectorAll(".contact-label");
 
@@ -66,4 +81,48 @@ contactInputAll.forEach((contactInput) => {
       label.classList.add("contact-focus-js");
     }
   });
+});
+
+// HERO PLAY VIDEO BTN
+
+const videoOverlay = document.createElement("div");
+const heroCta = document.querySelector(".play-video-btn");
+
+heroCta.addEventListener("click", (e) => {
+  // Create a div for the video overlay
+  videoOverlay.classList.add("hero-overlay");
+
+  // Create the video element and set its attributes
+  const videoClip = document.createElement("video");
+  videoClip.src = "/src/assets/konig-pferd-ovelray-video.mp4";
+  videoClip.autoplay = true;
+  videoClip.loop = true;
+  videoClip.controls = true;
+  videoClip.muted = false;
+  videoClip.poster = "/src/assets/images/thumbnail-splash-overlay.jpg";
+  videoClip.preload = "auto";
+  videoClip.crossOrigin = "anonymous";
+  videoClip.playsInline = true;
+  videoClip.disableRemotePlayback = true;
+  videoClip.classList.add("modal-video");
+
+  // Create the source element and set its attributes
+  const sourceElement = document.createElement("source");
+  sourceElement.src = "video.mp4";
+  sourceElement.type = "video/mp4";
+
+  // Append the source element to the video element
+  videoClip.appendChild(sourceElement);
+
+  // Append the video element to the video overlay div
+  videoOverlay.append(videoClip);
+
+  // Append the video overlay div to the body
+  document.body.append(videoOverlay);
+});
+videoOverlay.addEventListener("click", (e) => {
+  if (e.target.matches(".hero-overlay")) {
+    videoOverlay.firstElementChild.remove();
+    videoOverlay.remove();
+  }
 });
